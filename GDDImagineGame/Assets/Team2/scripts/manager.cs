0388505767a2095883935be203c6f37ab0af37c4
dantayy <nvm6501@rit.edu>
2019-10-14 19:01:05 -0400

    Created structural folders for holding our assets, added manager game object to hold the manager script in, and added a rudimentary timing system to that manager script

---
+++ GDDImagineGame/Assets/Team2/scripts/manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour
+{
+
+    //private var for keeping track of the time
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check to see if time is up for picking an action
+        if (timer >= 5.0f)
+        {
+            timer = 0.0f;
+            //Resolve player choices here
+            //After resolving choices and updating scores, check to see if the game would end here
+        }
+        else //default case, increment
+        {
+            timer += Time.deltaTime;
+            /*
+             Code for listening to player input and preventing them from making another choice would go here
+             Use public game objects that can be set to specific object instances in the unity editor
+             to set the variables that would be checked here
+             */
+        }
+    }
+}
