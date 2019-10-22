6282062c53ed9a30b8aaf3ba98976d18281db177
Cavan Vince <csvince@charter.net>
2019-10-21 21:49:49 -0400

    Added a Player Controller script that checks for one of three button presses to determine the player's action.

---
+++ GDDImagineGame/Assets/Team2/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerAction();
+    }
+
+    /// <summary>
+    /// Check for user input and print a message on the console that corresponds with the key that the user pressed
+    /// </summary>
+    public void PlayerAction()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Go For Main Pot");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Steal");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Block");
+        }
+    }
+}
