16dbf579cd10da2e03e0dbee5537968d614bc1ef
SkyFavor <delprince979@users.noreply.github.com>
2019-11-08 21:17:29 -0500

    Added collidables and point system

---
+++ GDDImagineGame/Assets/Team3/Scripts/AnthonyScripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : Collidable
+{
+    public override void OnCollide(GameObject other)
+    {
+        GameObject.Find("Player").GetComponent<CharacterController>().score += 1;
+        GameObject.Find("PointText").GetComponent<Text>().text = "Points: " + GameObject.Find("Player").GetComponent<CharacterController>().score;
+    }
+}
---
+++ GDDImagineGame/Assets/Team3/Scripts/AnthonyScripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Obstacle : Collidable
+{
+
+    public override void OnCollide(GameObject other)
+    {
+        GameObject.Find("Player").GetComponent<CharacterController>().score -= 1;
+        GameObject.Find("PointText").GetComponent<Text>().text = "Score: " + GameObject.Find("Player").GetComponent<CharacterController>().score;
+    }
+
+}
