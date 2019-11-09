1c05e929eb77fe938d730dd962f0b0605e2a0c4a
Nic Lande (RIT Student) <ndl1391@ad.rit.edu>
2019-11-08 22:19:41 -0500

    added to Dart script

--- GDDImagineGame/Assets/Team1/Scripts/Dart.cs
+++ GDDImagineGame/Assets/Team1/Scripts/Dart.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 public class Dart : MonoBehaviour
 {
     private Vector3 velocity;
+    private GameObject body;
+    private Player thrower;
+
+    public Player Thrower
+    {
+        get
+        {
+            return thrower;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        body = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        body.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(velocity);
+        body.
     }
 
     /// <summary>
-    /// called upon creation of the dart to initialize its velocity
+    /// Throws the dart upon the player's command
     /// </summary>
-    /// <param name="xVal">the x value returned by the controller</param>
-    /// <param name="yVal">the y value returned by the controller</param>
-    public void Throw(float xVal, float yVal)
+    /// <param name="xVal">the X Value of the dart's velocity</param>
+    /// <param name="yVal">the Y Value of the dart's velocity</param>
+    /// <param name="player">the player that threw the dart</param>
+    public void Throw(float xVal, float yVal, Player player)
     {
         velocity = new Vector3(xVal, yVal, 0);
+        thrower = player;
     }
 }
