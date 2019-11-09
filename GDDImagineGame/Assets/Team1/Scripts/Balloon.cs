86ceb332feff79653f9caa5950a1434b120d189b
John <j.yakubison@gmail.com>
2019-11-08 23:48:08 -0500

    Added Multi Colored Balloon Spawns

--- GDDImagineGame/Assets/Team1/Scripts/Balloon.cs
+++ GDDImagineGame/Assets/Team1/Scripts/Balloon.cs
@@ -32,23 +32,24 @@ public class Balloon : MonoBehaviour
     void Start()
     {
         balloonSpeed = Random.Range(lowerSpeedRange, upperSpeedRange);
-        
-        //material = gameObject.GetComponent<Renderer>().material;
-        int randColor = Random.Range(1, 4);
-        switch (randColor)
-        {
-            case 1:
-                material.SetColor("red", Color.red);
-                break;
-            case 2:
-                material.SetColor("blue", Color.blue);
-                break;
-            case 3:
-                material.SetColor("green", Color.green);
-                break;
 
-        }
-        gameObject.GetComponent<Renderer>().material = material;
+        ColorSelection();
+       // material = gameObject.GetComponent<Renderer>().material;
+       // int randColor = Random.Range(1, 4);
+       //switch (randColor)
+       //{
+       //    case 1:
+       //        material.SetColor("red", Color.red);
+       //        break;
+       //    case 2:
+       //        material.SetColor("blue", Color.blue);
+       //        break;
+       //    case 3:
+       //        material.SetColor("green", Color.green);
+       //        break;
+       //
+       //}
+       //gameObject.GetComponent<Renderer>().material = material;
         points = 100;
         cManage = FindObjectOfType<CollisionManager>();
     }
@@ -89,6 +90,23 @@ public class Balloon : MonoBehaviour
                 hit.collider.gameObject.GetComponent<Balloon>().BalloonSpeed = balloonSpeed;
                 balloonSpeed = tempBalloonSpeed;
             }
+        }
+    }
+
+    void ColorSelection()
+    {
+        int colorValue = Random.Range(1, 4);
+        switch (colorValue)
+        {
+            case 1:
+                GetComponent<MeshRenderer>().material.color = Color.red;
+                break;
+            case 2:
+                GetComponent<MeshRenderer>().material.color = Color.blue;
+                break;
+            case 3:
+                GetComponent<MeshRenderer>().material.color = Color.green;
+                break;
         }
     }
 
