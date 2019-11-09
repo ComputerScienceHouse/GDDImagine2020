9d95a3306a0da2d6ccc1404dd0119ef64964e021
Ricky Guerin <Guerin.Ricky@gmail.com>
2019-11-09 15:04:21 -0500

    General clean up

--- GDDImagineGame/Assets/Team2/scripts/PlayerController.cs
+++ GDDImagineGame/Assets/Team2/scripts/PlayerController.cs
@@ -1,26 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-//enum for choices a player can make
+// Enumeration of the actions a player can take in a round
 public enum Choice
 {
-    pot,
-    stealLeft,
-    stealAcross,
-    stealRight,
-    block,
-    none
+    Pot,
+    StealLeft,
+    StealAcross,
+    StealRight,
+    Block,
+    None
 }
 
 public class PlayerController : MonoBehaviour
 {
-
-    //vars for players
+	// Player's current score
     public int score;
-    //public bool animating;
-    //public bool firstHalf;
+
+	// Player's action for this round
     public Choice choice;
+
+	// The keys this player must press to perform various actions
     public KeyCode potButton;
     public KeyCode stealButton;
     public KeyCode blockButton;
@@ -28,46 +27,31 @@ public class PlayerController : MonoBehaviour
     public KeyCode stealAcross;
     public KeyCode stealRight;
 
-    // Start is called before the first frame update
     void Start()
     {
         //initialize some vars
         score = 0;
-        //animating = false;
-        //firstHalf = true;
-        choice = Choice.none;
+        choice = Choice.None;
     }
 
-    // Update is called once per frame
     void Update()
     {
         PlayerAction();
-        //if (animating)
-        //{
-        //    if (firstHalf)
-        //    {
-
-        //    }
-        //    else
-        //    {
-
-        //    }
-        //}
     }
 
-    /// <summary>
-    /// Check for user input and print a message on the console that corresponds with the key that the user pressed
-    /// </summary>
-    public void PlayerAction()
+	///
+	/// Check for user input and print a message on the console that corresponds with the key that the user pressed
+	///
+	public void PlayerAction()
     {
         //only let player make choice if none have been made yet
-        if (choice == Choice.none)
+        if (choice == Choice.None)
         {
             //player wants to go for pot
             if (Input.GetKeyDown(potButton))
             {
                 Debug.Log(name + " is going for main pot");
-                choice = Choice.pot;
+                choice = Choice.Pot;
             }
             //player wants to go for a steal, handle who they're trying to steal from
             else if (Input.GetKey(stealButton))
@@ -75,57 +59,25 @@ public class PlayerController : MonoBehaviour
                 if (Input.GetKeyDown(stealLeft))
                 {
                     Debug.Log(name + " is stealing from the player to the left of them");
-                    choice = Choice.stealLeft;
+                    choice = Choice.StealLeft;
                 }
                 else if (Input.GetKeyDown(stealAcross))
                 {
                     Debug.Log(name + " is stealing from the player across from them");
-                    choice = Choice.stealAcross;
+                    choice = Choice.StealAcross;
                 }
                 else if (Input.GetKeyDown(stealRight))
                 {
                     Debug.Log(name + " is stealing from the player to the right of them");
-                    choice = Choice.stealRight;
+                    choice = Choice.StealRight;
                 }
             }
             //player wants to block
             else if (Input.GetKeyDown(blockButton))
             {
                 Debug.Log(name + " is going for blocking");
-                choice = Choice.block;
+                choice = Choice.Block;
             }
         }
     }
-
-    ////helper function for a successful pot steal animation
-    //public void PlayerMoveToPotSuccessFul()
-    //{
-    //    animating = true;
-    //}
-
-    ////helper function for a unsuccessful pot steal animation
-    //public void PlayerMoveToPotUnsuccessFul()
-    //{
-    //    animating = true;
-    //}
-
-    ////helper function for a unsuccessful pot steal animation
-    //public void PlayerMoveToStealSuccessFul(float degreesToRotate)
-    //{
-    //    animating = true;
-
-    //}
-
-    ////helper function for a unsuccessful pot steal animation
-    //public void PlayerMoveToStealUnsuccessFul(float degreesToRotate)
-    //{
-    //    animating = true;
-    //}
-
-    ////helper function for a block animation
-    //public void PlayerMoveToBlock()
-    //{
-    //    animating = true;
-
-    //}
 }
--- GDDImagineGame/Assets/Team2/scripts/manager.cs
+++ GDDImagineGame/Assets/Team2/scripts/manager.cs
@@ -1,178 +1,155 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-
-    //private var for keeping track of the time
-    public float timer = 0.0f;
+	// Provides access to all four players
+	public PlayerController[] players;
 
 	// The Text object that displays the time remaining in the round
 	public Text timerText;
 
-    //public var for how long a "round" should be (time when players can choose an action)
+    // Duration in seconds of one game round
     public float timeToChoose;
 
-    //public vars for managing the 4 players
-    public GameObject player1;
-    public GameObject player2;
-    public GameObject player3;
-    public GameObject player4;
+	// The current value of the pot
+	public int potValue;
 
-    //public var for the pot value
-    public int potValue;
+	// The amount by which the pot increases value after each round
     public int potIncrease;
 
-    //vars for rounds
+    // The total number of rounds in the game
     public int numRounds;
+
+	// The current round number
     private int roundNum;
+
+	// If the game is over, prevent Update() from happening
     private bool gameOver;
 
-    //var for checking if players are animating after a round has been calculated
-    private bool playersAnimating;
-
-    private PlayerController[] players;
-
-    // Start is called before the first frame update
+	// Keeps track of elapsed game time
+	private float timer;
+	
     void Start()
     {
-        //initialize some vars
-        players = new PlayerController[] { player1.GetComponent<PlayerController>(), player2.GetComponent<PlayerController>(), player3.GetComponent<PlayerController>(), player4.GetComponent<PlayerController>() };
+		timer = 0;
         roundNum = 1;
         gameOver = false;
-        playersAnimating = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        ////make sure animations aren't happening at the moment
-        //if (!players[0].animating && !players[1].animating && !players[2].animating && !players[3].animating)
-        //{
-        //check to see if time is up for picking an action
-        if (timer >= timeToChoose && !gameOver)
-        {
-            //reset timer
-            timer = 0.0f;
-            //Resolve player choices here
+		// Prevent Update() when the game has ended
+		if (gameOver) return;
 
-            //condense players choices for checks
-            Choice[] choices = { players[0].choice, players[1].choice, players[2].choice, players[3].choice };
-
-            //temp vars for checking who is going to pot
-            int potNum = 0;
-            int potPlayerNum = -1;
-
-            //check players choices
-            for (int i = 0; i < choices.Length; i++)
-            {
-                //if pot, increase pot num and assign player
-                if (choices[i] == Choice.pot)
-                {
-                    potNum++;
-                    potPlayerNum = i;
-                }
-                //handle steals
-                else if (choices[i] == Choice.stealLeft || choices[i] == Choice.stealAcross || choices[i] == Choice.stealRight)
-                {
-                    int target = -1;
-                    float degreesToRotate = 0;
-                    switch (choices[i])
-                    {
-                        case Choice.stealLeft:
-                            target = (i - 1) % 4;
-                            degreesToRotate = -90;
-                            break;
-                        case Choice.stealAcross:
-                            target = (i + 2) % 4;
-                            degreesToRotate = 180;
-                            break;
-                        case Choice.stealRight:
-                            target = (i + 1) % 4;
-                            degreesToRotate = 90;
-                            break;
-                        default:
-                            break;
-                    }
-                    //ignore if the chosen player is blocking, otherwise give half of score to player stealing
-                    if (choices[target] != Choice.block)
-                    {
-                        int half = (int)Mathf.Ceil(players[target].score / 2);
-                        //players[i].PlayerMoveToStealSuccessFul(degreesToRotate);
-                        players[i].score += half;
-                        players[target].score -= half;
-                    }
-                    else
-                    {
-                        //players[i].PlayerMoveToStealUnsuccessFul(degreesToRotate);
-                    }
-                }
-            }
-            //if only 1 going for pot, give them pot value
-            if (potNum == 1)
-            {
-                //players[potPlayerNum].PlayerMoveToPotSuccessFul();
-                //players[potPlayerNum].animating = true;
-                players[potPlayerNum].score += potValue;
-            }
-            else if (potNum > 1)
-            {
-                foreach (PlayerController pc in players)
-                {
-                    if (pc.choice == Choice.pot)
-                    {
-                        //pc.PlayerMoveToPotUnsuccessFul();
-                    }
-                }
-            }
-
-            foreach (PlayerController pc in players)
-            {
-                if (pc.choice == Choice.block)
-                {
-                    //pc.PlayerMoveToBlock();
-                }
-            }
-
-            //After resolving choices and updating scores, check to see if the game would end here
-            //reset players choices and give them some pity money
-            foreach (PlayerController pc in players)
-            {
-                pc.choice = Choice.none;
-                pc.score += 1;
-                Debug.Log(pc.name + "'s score is: " + pc.score);
-            }
-            //increase pots value
-            potValue += potIncrease;
-
-            //increase round number
-            roundNum++;
-
-            //ends the game if max rounds reached
-            if (roundNum >= numRounds)
-            {
-                int highScorePlayer = -1;
-                int highScore = 0;
-                for (int i = 0; i < players.Length; i++)
-                {
-                    if (players[i].score > highScore)
-                    {
-                        highScore = players[i].score;
-                        highScorePlayer = i;
-                    }
-                }
-
-                Debug.Log("The winner is player " + (highScorePlayer + 1) + " with a score of " + highScore);
-                gameOver = true;
-            }
-        }
-        else //default case, increment
-        {
-            timer += Time.deltaTime;
+		// Increase timer
+		if (timer < timeToChoose)
+		{
+			timer += Time.deltaTime;
 			timerText.text = "Timer: " + Mathf.Round((timeToChoose - timer) * 100) / 100;
-        }
-        //}
+		}
+
+		// Once timer has reached timeToChoose, handle choices
+		else
+		{
+			// Collect Player choices
+			Choice[] choices = { players[0].choice, players[1].choice, players[2].choice, players[3].choice };
+
+			// Number of players who went for the pot
+			int potNum = 0;
+
+			// ID of player who went for the pot
+			int potPlayerNum = -1;
+
+			// Handle each choice
+			for (int i = 0; i < choices.Length; i++)
+			{
+				// Player went for the pot
+				if (choices[i] == Choice.Pot)
+				{
+					potNum++;
+					potPlayerNum = i;
+				}
+
+				// Player chose to steal
+				else if (choices[i] == Choice.StealLeft || choices[i] == Choice.StealAcross || choices[i] == Choice.StealRight)
+				{
+					int target = -1;
+
+					switch (choices[i])
+					{
+						case Choice.StealLeft:
+							target = (i - 1) % 4;
+							break;
+						case Choice.StealAcross:
+							target = (i + 2) % 4;
+							break;
+						case Choice.StealRight:
+							target = (i + 1) % 4;
+							break;
+					}
+
+					//ignore if the chosen player is blocking, otherwise give half of score to player stealing
+					if (choices[target] != Choice.Block)
+					{
+						int half = (int)Mathf.Ceil(players[target].score / 2);
+						players[i].score += half;
+						players[target].score -= half;
+					}
+				}
+			}
+
+			// Only one player went for the pot, give them pot value
+			if (potNum == 1)
+			{
+				players[potPlayerNum].score += potValue;
+			}
+
+			//After resolving choices and updating scores, check to see if the game would end here
+			//reset players choices and give them some pity money
+			foreach (PlayerController pc in players)
+			{
+				pc.choice = Choice.None;
+				pc.score += 1;
+				Debug.Log(pc.name + "'s score is: " + pc.score);
+			}
+
+			// Increase round number
+			roundNum++;
+
+			// End game if round number is greater than number of rounds
+			if (roundNum > numRounds)
+			{
+				GameOver();
+				return;
+			}
+
+			// Increase pot value
+			potValue += potIncrease;
+
+			// Reset timer
+			timer = 0.0f;
+		}
     }
+
+	///
+	/// When the game has ended, show results
+	///
+	private void GameOver()
+	{
+		int highScorePlayer = -1;
+		int highScore = 0;
+		for (int i = 0; i < players.Length; i++)
+		{
+			if (players[i].score > highScore)
+			{
+				highScore = players[i].score;
+				highScorePlayer = i;
+			}
+		}
+
+		timerText.text = "Game Over!";
+		Debug.Log("The winner is player " + (highScorePlayer + 1) + " with a score of " + highScore);
+		gameOver = true;
+	}
 }
