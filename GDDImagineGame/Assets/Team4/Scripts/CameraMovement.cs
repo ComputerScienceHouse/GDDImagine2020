6bad70feebe6204a176ba7f54045552ddabf63a3
brp8396 <brp8396@rit.edu>
2020-03-04 00:11:26 -0500

    Added camera lerping

--- GDDImagineGame/Assets/Team4/Scripts/CameraMovement.cs
+++ GDDImagineGame/Assets/Team4/Scripts/CameraMovement.cs
@@ -2,6 +2,10 @@
 
 public class CameraMovement : MonoBehaviour
 {
+    private Vector3 camStart;
+    private Vector3 camEnd;
+    private Vector3 lightStart;
+    private Vector3 lightEnd;
     // Light Prefab
     public GameObject LightPrefab;
 
@@ -16,6 +20,21 @@ public class CameraMovement : MonoBehaviour
         players = GameObject.FindGameObjectsWithTag("Player");
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
         lightObject = Instantiate(LightPrefab);
+
+        float x = 0, z = 0;
+        foreach (GameObject player in players)
+        {
+            x += player.transform.position.x;
+            z += player.transform.position.z;
+        }
+
+        foreach (GameObject enemy in enemies)
+        {
+            x += enemy.transform.position.x;
+            z += enemy.transform.position.z;
+        }
+
+        transform.position = new Vector3(x / 4, 40, z / 4 - 50);
     }
 
     // Update is called once per frame
@@ -35,9 +54,17 @@ public class CameraMovement : MonoBehaviour
             z += enemy.transform.position.z;
         }
 
-        // Move the camera and light based on this 
-        transform.position = new Vector3(x / 4, 40, z / 4 - 50);
-        lightObject.transform.position = new Vector3(x / 4, 40, z / 4);
+        // Define start and end vectors here
+        camStart = transform.position;
+        lightStart = lightObject.transform.position;
+        camEnd = new Vector3(x / 4, 40, z / 4 - 50);
+        lightEnd = new Vector3(x /4, 40, z / 4);
 
+        // Move the camera and light based on this
+        //transform.position = new Vector3(x / 4, 40, z / 4 - 50);
+        //lightObject.transform.position = new Vector3(x / 4, 40, z / 4);
+
+        transform.position = Vector3.Lerp(camStart, camEnd, 0.05f);
+        lightObject.transform.position = Vector3.Lerp(lightStart, lightEnd, 0.05f);
     }
 }
